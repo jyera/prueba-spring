aeaa9a23087ae9afb91af8fba967344abec64e76
SrPepperoni <jaimeyera@gmail.com>
2017-08-10 22:23:34 +0200

    Primer Commit
    Primer commit del proyecto prueba-spring

---
+++ src/main/java/com/ejemplo/pruebaspring/PruebaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.ejemplo.pruebaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ejemplo/pruebaspring/PruebaSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ejemplo.pruebaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PruebaSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
